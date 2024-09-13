52819305597c0a9e1b5b8f4b290a91345b47e85a
Kenny Baltazar alanoca <kenny@iMac-de-Kenny.local>
2024-09-13 07:32:33 -0500

    url added

--- src/main/java/com/training/shell/trainingjpa/ui/controllers/StudentNotReactiveController.java
+++ src/main/java/com/training/shell/trainingjpa/ui/controllers/StudentNotReactiveController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
+// http://localhost:9001/students_not_reactive/fetch
 @AllArgsConstructor
 @RestController
 @RequestMapping("/students_not_reactive")
